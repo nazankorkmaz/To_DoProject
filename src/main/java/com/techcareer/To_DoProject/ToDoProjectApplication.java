c9e5819dfd11634e69877907134d5d98046739d6
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2024-05-14 17:34:46 +0300

    init2

--- src/main/java/com/techcareer/To_DoProject/ToDoProjectApplication.java
+++ src/main/java/com/techcareer/To_DoProject/ToDoProjectApplication.java
@@ -1,12 +1,33 @@
 package com.techcareer.To_DoProject;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+//@EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme ancak Spring security için gerekli kütüphaneleri dahil
+
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}    //burası projeyi ayağa kaldıran kısım---------------------------------
+)
+
+//@SpringBootApplication
 public class ToDoProjectApplication {
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
 
 	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "false");
+
 		SpringApplication.run(ToDoProjectApplication.class, args);
 	}
 
